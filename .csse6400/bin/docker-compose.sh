0f687a8d5416ab0b2a047f7b2c7cf8a6dab08470
Jordan <rike568@gmail.com>
2025-03-17 10:58:47 +1000

    Modified compose file for new version of docker compose

--- .csse6400/bin/docker-compose.sh
+++ .csse6400/bin/docker-compose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # Check that the health endpoint is returning 200 using docker-compose
-docker-compose build
-docker-compose up -d
+docker compose build
+docker compose up -d
 error=$?
 pid=$!
 if [[ $error -ne 0 ]]; then
@@ -21,5 +21,5 @@ if [[ $error -ne 0 ]]; then
     exit 1
 fi
 
-docker-compose down
+docker compose down
 
